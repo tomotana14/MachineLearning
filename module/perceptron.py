72e4ce5d3c745d5983b670d803a95f473902c74f
tomotana14 <tomotana14@gmail.com>
2013-10-20 18:48:29 +0900

    initial commit

---
+++ module/perceptron.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#predict
+def predict(wvec, xmat):
+    out = np.dot(wvec, xvec)
+    if out >= 0:
+        res = 1
+    else:
+        res = -1
+    return [res, out]
+
+#train
+def train(wvec, xmat, label):
+    [res,out] = predict(wvec,xmat)
+    c = 0.5
+    if out * label < 0:
+        wtmp = wvec + c * label * xvec
+        return wtmp
+    else:
+        return wvec
+
+
+if __name__ == '__main__':
+    item_num = 100
+    loop = 1
+    init_wvec = [1,-1,1]
+
+    x1_1 = np.ones(int(item_num/2)) + 10*np.random.random(int(item_num/2))
+    x1_2 = np.ones(int(item_num/2)) + 10*np.random.random(int(item_num/2))
+    x2_1 = np.ones(int(item_num/2)) - 10*np.random.random(int(item_num/2))
+    x2_2 = np.ones(int(item_num/2)) - 10*np.random.random(int(item_num/2))
+    z = np.ones(int(item_num/2))
+
+    x1 = np.c_[x1_1,x1_2,z]
+    x2 = np.c_[x2_1,x2_2,z]
+    class_x = np.array(np.r_[x1,x2])
+
+    label1 = np.ones(int(item_num/2))
+    label2 = -1 * np.ones(int(item_num/2))
+    label_x = np.array(np.r_[label1, label2])
+
+    wvec = np.vstack((init_wvec, init_wvec))
+
+    for j in range(loop):
+        for i in range(item_num):
+            wvec_new = train(wvec[-1],class_x[i,:], label_x[i])
+            wvec = np.vstack((wvec,wvec_new))
+    w = wvec[-1]
+    print w
+
+    x_fig = range(-15, 16)
+    y_fig = [-(w[1]/w[0])*xi-(w[2]/w[1]) for xi in x_fig]
+
+    plt.scatter(x1[:,0],x1[:,1],marker='o',color='g',s=100)
+    plt.scatter(x2[:,0],x2[:,1],marker='s',color='b',s=100)
+    plt.plot(x_fig,y_fig)
+    plt.show()
