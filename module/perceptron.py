7b6086d7a78c33345762a8e69914162ab9968f1e
tomotana14 <tomotana14@gmail.com>
2013-10-20 23:42:23 +0900

    mod

--- module/perceptron.py
+++ module/perceptron.py
@@ -6,17 +6,16 @@ import numpy as np
 #predict
 def predict(wvec, xmat):
     out = np.dot(xmat, wvec)
-    res = np.ones((xmat.shape[0], 1))
+    res = np.ones(xmat.shape[0])
     negative = np.where(out < 0)
     res[negative] = -1
     return [res, out]
 
 #train
-def train(wvec, xmat, label):
+def train(wvec, xmat, label, cof):
     [res, out] = predict(wvec,xmat)
-    c = 0.5
     tmp_vec = out * label
     negative = np.where(tmp_vec < 0)
     if len(negative[0]) > 0:
         for i in negative[0]:
-            wvec += c * label[i] * xmat[i]
+            wvec += cof * label[i] * xmat[i]
