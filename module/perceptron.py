150e79152ad36062534b1de6ac869278ad685e4b
tomotana14 <tomotana14@gmail.com>
2013-10-29 23:17:59 +0900

    fixed

--- module/perceptron.py
+++ module/perceptron.py
@@ -19,3 +19,20 @@ def train(wvec, xmat, label, cof):
     if len(negative[0]) > 0:
         for i in negative[0]:
             wvec += cof * label[i] * xmat[i]
+
+if __name__ == '__main__':
+    num_train = 500
+    cof = 0.1
+    x1 = np.random.randint(0,10,(10,2))
+    x2 = np.random.randint(10,20,(10,2))
+    x = np.r_[x1,x2]
+    x = np.c_[x, np.ones(20)]
+
+    label = np.ones(20)
+    label[10:] = -1
+    w = np.random.random(3)
+    for i in range(num_train):
+        train(w, x, label, cof)
+
+    res, out = predict(w, x)
+    print res;
